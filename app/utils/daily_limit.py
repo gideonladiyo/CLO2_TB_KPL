b5d7b9b0c5d31d969cde3d04397f804dc6af893b
Devrin Anggun Saputri <devrinanggunsaputri90@gmail.com>
2025-04-29 15:31:25 +0700

    Menambahkan  isi di dalam class folder utils

--- app/utils/daily_limit.py
+++ app/utils/daily_limit.py
@@ -1,4 +1,19 @@
+from datetime import datetime, date
+from fastapi import HTTPException
+from app.config import settings
 from typing import List
 
 def check_limit(orders: list):
-    return 
+    count = 0
+    today = date.today()
+    for order in orders:
+        try:
+            order_date = datetime.strptime(order["created_at"], "%Y-%m-%d %H:%M:%S.%f").date()
+            if order_date == today:
+                count += 1
+        except (KeyError, ValueError):
+            continue
+    if count == settings.max_orders_per_day:
+        raise HTTPException(status_code=429, detail="Order sudah mencapai limit")
+
+
--- app/utils/generate_id.py
+++ app/utils/generate_id.py
@@ -1,5 +1,8 @@
 from typing import List
+import random
 
 def generate_random_id(ids: List, startswith: str, digit_number: int) -> str:
-    result = ""
-    return result
+    new_id = startswith + str(random.randint(0, 99999)).zfill(digit_number)
+    if new_id in ids:
+        return generate_random_id(ids=ids, startswith=startswith, digit_number=digit_number)
+    return new_id
