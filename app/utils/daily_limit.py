ba040dcdfa2e52f7ee1026ef7ec1f6fdff4b8fc3
Gideon Ladiyo <alwaysfun0604@gmail.com>
2025-05-01 11:01:56 +0700

    fix daily limit bug

--- app/utils/daily_limit.py
+++ app/utils/daily_limit.py
@@ -5,8 +5,13 @@ from typing import List
 def check_limit_order(orders_date: List, max_order:int):
     count = 0
     today = date.today()
+    print(f"today date: {today}")
+    print(f"max order: {max_order}")
     for order_date in orders_date:
         try:
+            order_date = order_date.split(" ")[0]
+            order_date = datetime.strptime(order_date, "%Y-%m-%d").date()
+            print(f"order date: {order_date}")
             if order_date == today:
                 count += 1
         except (KeyError, ValueError):
