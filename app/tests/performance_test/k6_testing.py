de319025922eae691c0b27b8b8eeba1e8bbf1d17
Gideon Ladiyo <alwaysfun0604@gmail.com>
2025-05-25 23:25:03 +0700

    update testing

--- app/tests/performance_test/k6_testing.py
+++ app/tests/performance_test/k6_testing.py
@@ -4,13 +4,15 @@ import json
 import os
 
 filepath = os.path.normpath(os.path.join(os.path.dirname(__file__), "..", "..", "data", "orders.json"))
-
+item_path = os.path.normpath(os.path.join(os.path.dirname(__file__), "..", "..", "data", "items.json"))
 
 def run_k6_test():
     print("Menjalankan k6 test...")
 
     with open (filepath, "r") as f:
         backup_orders = json.load(f)
+    with open (item_path, "r") as f:
+        backup_items = json.load(f)
 
     result = subprocess.run(
         ["k6", "run", "app/tests/performance_test/test_api_performance.js"], capture_output=True, text=True
@@ -19,6 +21,8 @@ def run_k6_test():
 
     with open(filepath, "w") as f:
         json.dump(backup_orders, f, indent=4)
+    with open(item_path, "w") as f:
+        json.dump(backup_items, f, indent=4)
 
     if result.returncode != 0:
         print("K6 test gagal:", result.stderr)
